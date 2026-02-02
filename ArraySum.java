4b37755a41cb9c77eb13e028bee03abd7593033b
Sri Vaishnavi <srivaishnavi.ganesan@gmail.com>
2026-02-02 22:27:59 +0530

    array-sum-done

--- ArraySum.java
+++ ArraySum.java
@@ -2,23 +2,26 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class ArraySum {
-    public static void main(String[] args) {
-        int sum=0;
+    public static void main(String args[]) {
+        int sum = 0;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the length of the array: ");
-        int length= sc.nextInt();
+        System.out.println("Enter the length");
+        int length = sc.nextInt();
+        // System.out.println(length);
+
         int[] arr = new int[length];
         for (int i = 0; i < arr.length; i++) {
-            int a=sc.nextInt();
-            arr[i]=a;
-            sum+=a;
-            
+            System.out.println("enter number" + (i+1) + ":");
+            int a = sc.nextInt();
+            arr[i] = a;
+            sum += a;
         }
+
         System.out.println(Arrays.toString(arr));
-        System.out.println(sum);
+
+        System.out.println("Sum" + sum);
+
         sc.close();
     }
-    
-}
 
-// gejb
+}
